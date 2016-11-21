e24d93109859eb796e98aaeb44bcfb2533a61cf6
Thimo Laptop <tkoolen1@student.avans.nl>
2016-11-21 10:11:45 +0100

    Fixed main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
 		System.out.println("Hello Yakup!");
 		System.out.println("Sven dit hoort gewoon te werken.");
 		System.out.println("Dus het wordt lekker chillen met netflix");
+		System.out.println("Ja man. Gezellig, maar wel zonder Yakup hoor.");
 	}
 }
