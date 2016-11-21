e39ca67f536e14fa5247bd9b233fd5895767de71
Sven Westerlaken <sven.westerlaken@hotmail.nl>
2016-11-21 11:02:40 +0100

    First commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,7 +8,7 @@ public class Main extends JFrame {
 		JFrame frame = new Main();
 		frame.setBounds(100, 100, 1000, 750);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Statistix and chill");
+		frame.setTitle("Statistix and chill + test");
 		frame.setContentPane(new MainPanel());
 		frame.setVisible(true);
 	}
