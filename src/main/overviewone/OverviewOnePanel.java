0fd30bcde32300e5e791055bbc741d5166739e8f
Thimo Laptop <tkoolen1@student.avans.nl>
2016-12-07 12:42:28 +0100

    Test

--- src/main/overviewone/OverviewOnePanel.java
+++ src/main/overviewone/OverviewOnePanel.java
@@ -2,6 +2,10 @@ package main.overviewone;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 public class OverviewOnePanel extends JPanel {
 	private JComboBox combox;
@@ -9,7 +13,20 @@ public class OverviewOnePanel extends JPanel {
 	public OverviewOnePanel() {
 		String[] Database = {"Abonnoment","Profiel","Voortgang","Programma","Aflevering","Serie","Film"};
 		combox = new JComboBox(Database);
+		ComboListener cl = new ComboListener();
+		combox.addItemListener(cl);
 
 		add(combox);
+	}
+	
+	class ComboListener implements ItemListener {
+		@Override
+		public void itemStateChanged(ItemEvent e) {
+			if (e.getStateChange() == ItemEvent.SELECTED) {
+				Object item = e.getItem();
+				
+				System.out.println("f: " + item.toString());
+			}
+		}
 	}
 }
