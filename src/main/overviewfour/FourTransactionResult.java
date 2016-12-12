534a59c11f09c21e4a47a511300ac20a8d9f77e5
Thimo Laptop <tkoolen1@student.avans.nl>
2016-12-12 09:22:27 +0100

    Panel 4 data

--- src/main/overviewfour/FourTransactionResult.java
+++ src/main/overviewfour/FourTransactionResult.java
@@ -1,4 +1,17 @@
 package main.overviewfour;
 
 public class FourTransactionResult {
+	private String format;
+	
+	public FourTransactionResult() {
+		format = "";
+	}
+	
+	public void setFormat(String format) {
+		this.format = format;
+	}
+	
+	public String displayFormat() {
+		return this.format;
+	}
 }
--- src/main/overviewfour/FourTransactionScript.java
+++ src/main/overviewfour/FourTransactionScript.java
@@ -1,4 +1,36 @@
 package main.overviewfour;
 
+import main.util.Connectable;
+
+import java.sql.ResultSet;
+
 public class FourTransactionScript {
+	
+	public static FourTransactionResult getInformation() {
+		FourTransactionResult result = new FourTransactionResult();
+		
+		String query = "SELECT programma.Titel" +
+				" FROM programma" +
+				" INNER JOIN film" +
+				" ON film.ID = programma.ID" +
+				" WHERE film.Leeftijdsindicatie < 16" +
+				" ORDER BY programma.Tijdsduur DESC;";
+		
+		ResultSet rs = Connectable.executeQuery(query);
+		String format = "";
+		
+		if (rs != null) {
+			try {
+				while (rs.next()) {
+					format += rs.getString("programma.Titel");
+					format += "/";
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			result.setFormat(format.substring(0, format.length() - 1));
+		}
+		
+		return result;
+	}
 }
